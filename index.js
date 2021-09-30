3e6b1e2ee2a6dc857fe4923438762eaeddebbb72
Miguel Lipa <miguellipa1@gmail.com>
2021-09-30 19:36:10 -0400

    validacion de email resuelto

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const inputs = document.querySelectorAll('#formulario input');
 const expresion = {
     nombre : /^[a-zA-Z\s]{1,40}$/,
     apellido : /^[a-zA-Z\s]{1,40}$/,
-    correo : /^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[a-zA-Z]$/,
+    // correo : /^[a-zA-Z0-9._]{5,64}@(?:[a-zA-Z0-9]\.){3,64}[a-zA-Z]{2,16}$/i,
+    correo : /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
     clave : /^.{4,16}$/
 }
 
@@ -15,7 +16,7 @@ const validarformulario = (e) => {
                 document.getElementById('grupo__nombre').classList.remove('grupo__formulario-incorrecto');
                 document.getElementById('grupo__nombre').classList.add('grupo__formulario-correcto');
                 document.querySelector('#grupo__nombre .mensaje-error').classList.remove('mensaje-error-activo');
-                
+                console.log("se ejecuto 1");
             }else {
                 document.getElementById('grupo__nombre').classList.add('grupo__formulario-incorrecto');
                 document.getElementById('grupo__nombre').classList.remove('grupo__formulario-correcto');
@@ -27,6 +28,7 @@ const validarformulario = (e) => {
                 document.getElementById('grupo__apellido').classList.remove('grupo__formulario-incorrecto');
                 document.getElementById('grupo__apellido').classList.add('grupo__formulario-correcto');
                 document.querySelector('#grupo__apellido .mensaje-error').classList.remove('mensaje-error-activo');
+                console.log("se ejecuto 2");
             }else {
                 document.getElementById('grupo__apellido').classList.add('grupo__formulario-incorrecto');
                 document.getElementById('grupo__apellido').classList.remove('grupo__formulario-correcto');
@@ -34,11 +36,14 @@ const validarformulario = (e) => {
             }
         break;
         case "correo":
+            console.log(e.target.name);
             if (expresion.correo.test(e.target.value)){
+                console.log("entra if");
                 document.getElementById('grupo__correo').classList.remove('grupo__formulario-incorrecto');
                 document.getElementById('grupo__correo').classList.add('grupo__formulario-correcto');
                 document.querySelector('#grupo__correo .mensaje-error').classList.remove('mensaje-error-activo');
             }else {
+                console.log("entra else");
                 document.getElementById('grupo__correo').classList.add('grupo__formulario-incorrecto');
                 document.getElementById('grupo__correo').classList.remove('grupo__formulario-correcto');
                 document.querySelector('#grupo__correo .mensaje-error').classList.add('mensaje-error-activo');
@@ -62,5 +67,9 @@ inputs.forEach((input) => {
     input.addEventListener('keyup', validarformulario);
     input.addEventListener('blur', validarformulario);
 });
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
 
 
